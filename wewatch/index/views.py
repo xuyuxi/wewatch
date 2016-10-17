14d8f1908c949a647d5fdfd49887ebcae1397f95
xyx19890401@163.COM <123456@github>
2016-10-18 00:09:20 +0800

    add core app

--- wewatch/index/views.py
+++ wewatch/index/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from core.wechat import wechatSession
 import time, requests
 import re
-
+WcSession = wechatSession()
 def login(request):
-    session = requests.Session()
-    url = 'https://login.weixin.qq.com/jslogin'
-    params = {
-        'appid': 'wx782c26e4c19acffb',
-        'redirect_uri': 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage',
-        'fun': 'new',
-        'lang': 'en_US',
-        '_': int(time.time()),
-        }
-    r = session.get(url, params = params)
-    
-    regx = r'window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)";'
-    data = re.search(regx, r.text)
-    if data and data.group(1) == '200': 
-        uuid = data.group(2)
-        url = 'https://login.weixin.qq.com/qrcode/' + uuid
-	return render(request, 'login.html',{'url':url}) #login page
+    url = WcSession.get_QRImage_url()
+    uuid = WcSession.get_uuid()
+    return render(request, 'login.html',{'url':url,'uuid':uuid}) #login page
