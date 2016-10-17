7b95bc984491ef44a7c980149c5d7a18f29c40d0
xyx19890401@163.COM <123456@github>
2016-10-18 01:32:38 +0800

    add static

--- wewatch/wewatch/urls.py
+++ wewatch/wewatch/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
 	url(r'^index/',include('index.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] 
